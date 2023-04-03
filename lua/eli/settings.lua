eb7e6a0d8c5f2516ce785cef30dea94729f358e4
Eli Shafer <eshafer@axon.com>
2023-04-02 21:38:59 -0700

    add comments for suggested settings

--- lua/eli/settings.lua
+++ lua/eli/settings.lua
@@ -1,29 +1,29 @@
 -- settings.lua
 
 -- Global options
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
-vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"     -- Use the system clipboard
+vim.opt.mouse = "a"                   -- Enable mouse support
+vim.opt.termguicolors = true          -- Enable 24-bit color support
 
 -- Window-local options
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.number = true                 -- Show line numbers
+vim.opt.relativenumber = true         -- Show relative line numbers
+vim.opt.cursorline = true             -- Highlight the current line
+vim.opt.wrap = false                  -- Disable line wrapping
+vim.opt.scrolloff = 8                 -- Start scrolling 8 lines before edge
+vim.opt.splitright = true             -- Open vertical splits to the right
+vim.opt.splitbelow = true             -- Open horizontal splits below
 
 -- Buffer-local options
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.smartindent = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 300
-vim.opt.timeoutlen = 500
-vim.opt.showmode = false
+vim.opt.expandtab = true              -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4                -- Indent by 4 spaces
+vim.opt.tabstop = 4                   -- Set tab width to 4 spaces
+vim.opt.smartindent = true            -- Auto-indent new lines
+vim.opt.ignorecase = true             -- Ignore case when searching
+vim.opt.smartcase = true              -- Don't ignore case with capital letters
+vim.opt.updatetime = 300              -- Set faster update time
+vim.opt.timeoutlen = 500              -- Set faster timeout for mappings
+vim.opt.showmode = false              -- Don't show mode (use statusline)
 
 -- Strip Trailing Whitespace
 vim.cmd("autocmd! BufWritePre * %s/\\s\\+$//e")
